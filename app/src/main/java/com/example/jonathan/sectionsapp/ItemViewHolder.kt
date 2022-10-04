c54bee2449ccb58192aac5a9799db8ede9f7be00
Jonathan Giorgi Silveira <jonathan.silveira-ext@ab-inbev.com>
2022-10-03 22:28:11 -0300

    Pass itemView property to Item's bind function

--- app/src/main/java/com/example/jonathan/sectionsapp/ItemViewHolder.kt
+++ app/src/main/java/com/example/jonathan/sectionsapp/ItemViewHolder.kt
@@ -3,8 +3,8 @@ package com.example.jonathan.sectionsapp
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 
-open class ItemViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-    fun bind(item: Item<ItemViewHolder>, position: Int) {
-        item.bind(this, position)
+class ItemViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    fun bind(item: Item, position: Int) {
+        item.bind(itemView, position)
     }
 }
