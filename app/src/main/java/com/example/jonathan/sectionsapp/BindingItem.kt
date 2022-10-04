de768958d7efc624833da4d9fda3d206deca246c
Jonathan Giorgi Silveira <jonathan.silveira-ext@ab-inbev.com>
2022-10-03 22:35:23 -0300

    Item class implementation using ViewBinding

--- app/src/main/java/com/example/jonathan/sectionsapp/BindingItem.kt
+++ app/src/main/java/com/example/jonathan/sectionsapp/BindingItem.kt
@@ -1,16 +1,25 @@
 package com.example.jonathan.sectionsapp
 
+import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.viewbinding.ViewBinding
 
-abstract class BindingItem<VB : ViewBinding> : Item<BindingItemViewHolder<VB>> {
-    override fun createViewHolder(parent: ViewGroup): BindingItemViewHolder<VB> {
-        return BindingItemViewHolder(inflateViewGroup(parent))
+abstract class BindingItem<VB : ViewBinding> : Item {
+    override fun createViewHolder(parent: ViewGroup): ItemViewHolder {
+        return ItemViewHolder(inflateLayout(parent))
     }
 
-    override fun bind(holder: BindingItemViewHolder<VB>, position: Int) {
-        bind(holder.binding, position)
+    override fun inflateLayout(parent: ViewGroup): View {
+        return LayoutInflater.from(parent.context).inflate(
+            viewType(),
+            parent,
+            false
+        )
     }
-    abstract fun inflateViewGroup(parent: ViewGroup): VB
+    override fun bind(itemView: View, position: Int) {
+        bind(initViewBinding(itemView), position)
+    }
+    abstract fun initViewBinding(itemView: View): VB
     abstract fun bind(binding: VB, position: Int)
 }
