51f28658d3c1441af374e25c72bffb4f4fd385d4
Jonathan Giorgi Silveira <jonathan.silveira-ext@ab-inbev.com>
2022-10-20 23:42:52 -0300

    Add implementations of ItemDecoration

---
+++ app/src/main/java/com/example/jonathan/sectionsapp/component/GridItemDecorator.kt
@@ -0,0 +1,26 @@
+package com.example.jonathan.sectionsapp.component
+
+import android.graphics.Rect
+import android.view.View
+import androidx.annotation.DimenRes
+import androidx.recyclerview.widget.RecyclerView
+
+class GridItemDecorator(
+    @DimenRes private val top: Int,
+    @DimenRes private val bottom: Int,
+    @DimenRes private val start: Int,
+    @DimenRes private val end: Int
+): RecyclerView.ItemDecoration() {
+    override fun getItemOffsets(
+        outRect: Rect,
+        view: View,
+        parent: RecyclerView,
+        state: RecyclerView.State
+    ) {
+        val resources = view.resources
+        outRect.top = resources.getDimension(top).toInt()
+        outRect.bottom = resources.getDimension(bottom).toInt()
+        outRect.left = resources.getDimension(start).toInt()
+        outRect.right = resources.getDimension(end).toInt()
+    }
+}
