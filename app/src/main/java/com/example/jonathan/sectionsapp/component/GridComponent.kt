bb20a0473a71622ed54082d223e373752c1818c8
Jonathan Giorgi Silveira <jonathan.silveira-ext@ab-inbev.com>
2022-10-20 23:40:30 -0300

    Apply layout manager based on section config

--- app/src/main/java/com/example/jonathan/sectionsapp/component/GridComponent.kt
+++ app/src/main/java/com/example/jonathan/sectionsapp/component/GridComponent.kt
@@ -1,18 +1,18 @@
 package com.example.jonathan.sectionsapp.component
 
 import android.view.View
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.ItemDecoration
+import com.example.jonathan.component.ContainerViewHolderComponent
+import com.example.jonathan.domain.model.section.GridSection
 import com.example.jonathan.sectionsapp.R
 import com.example.jonathan.sectionsapp.databinding.GridItemBinding
 
 class GridComponent(
-    items: List<com.example.jonathan.component.ViewHolderComponent>? = null
-): com.example.jonathan.component.ContainerViewHolderComponent<GridItemBinding>() {
-
-    init {
-        if (!items.isNullOrEmpty())
-            addAll(items)
-    }
-
+    private val item: GridSection,
+    private val itemDecorator: ItemDecoration
+) : ContainerViewHolderComponent<GridItemBinding>() {
     override fun initViewBinding(itemView: View): GridItemBinding {
         return GridItemBinding.bind(itemView)
     }
@@ -20,6 +20,16 @@ class GridComponent(
     override fun viewType(): Int = R.layout.grid_item
 
     override fun bind(binding: GridItemBinding, position: Int) {
-        binding.gridItems.adapter = this.adapter
+        with(binding.gridItems) {
+            removeItemDecoration(itemDecorator)
+            addItemDecoration(itemDecorator)
+            layoutManager = GridLayoutManager(
+                context,
+                item.columnsCount,
+                RecyclerView.VERTICAL,
+                false
+            )
+            setAdapterTo(this)
+        }
     }
 }
