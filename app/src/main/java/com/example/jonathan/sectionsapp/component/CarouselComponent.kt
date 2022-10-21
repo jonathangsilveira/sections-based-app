7b0719e224b5c0d0d5fc554b57bfe128840eb9b0
Jonathan Giorgi Silveira <jonathan.silveira-ext@ab-inbev.com>
2022-10-20 23:35:17 -0300

    Change carousel component

--- app/src/main/java/com/example/jonathan/sectionsapp/component/CarouselComponent.kt
+++ app/src/main/java/com/example/jonathan/sectionsapp/component/CarouselComponent.kt
@@ -3,12 +3,14 @@ package com.example.jonathan.sectionsapp.component
 import android.view.View
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.ItemDecoration
+import com.example.jonathan.component.ContainerViewHolderComponent
 import com.example.jonathan.sectionsapp.R
 import com.example.jonathan.sectionsapp.databinding.CarouselItemBinding
 
 class CarouselComponent(
-    private val items: List<com.example.jonathan.component.ViewHolderComponent>
-): com.example.jonathan.component.ContainerViewHolderComponent<CarouselItemBinding>() {
+    private val itemDecoration: ItemDecoration
+) : ContainerViewHolderComponent<CarouselItemBinding>() {
     override fun initViewBinding(itemView: View): CarouselItemBinding {
         return CarouselItemBinding.bind(itemView)
     }
@@ -16,12 +18,15 @@ class CarouselComponent(
     override fun viewType(): Int = R.layout.carousel_item
 
     override fun bind(binding: CarouselItemBinding, position: Int) {
-        binding.carouselItems.layoutManager = LinearLayoutManager(
-            binding.root.context,
-            RecyclerView.HORIZONTAL,
-            false
-        )
-        binding.carouselItems.adapter = super.adapter
-        addAll(items)
+        with(binding.carouselItems) {
+            removeItemDecoration(itemDecoration)
+            addItemDecoration(itemDecoration)
+            layoutManager = LinearLayoutManager(
+                context,
+                RecyclerView.HORIZONTAL,
+                false
+            )
+            setAdapterTo(this)
+        }
     }
 }
