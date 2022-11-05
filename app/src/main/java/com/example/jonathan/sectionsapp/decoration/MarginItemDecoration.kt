75749b6b3a8aeef9fefc48c85d018b07b5720fd7
Jonathan Giorgi Silveira <jonathan.silveira-ext@ab-inbev.com>
2022-11-05 12:42:06 -0300

    Add secondary constructor with vertical and horizontal parameters. Also added open modifier to enable class to be extended

--- app/src/main/java/com/example/jonathan/sectionsapp/decoration/MarginItemDecoration.kt
+++ app/src/main/java/com/example/jonathan/sectionsapp/decoration/MarginItemDecoration.kt
@@ -4,13 +4,25 @@ import android.graphics.Rect
 import android.view.View
 import androidx.annotation.DimenRes
 import androidx.recyclerview.widget.RecyclerView
+import com.example.jonathan.sectionsapp.R
 
-class MarginItemDecoration(
+open class MarginItemDecoration constructor(
     @DimenRes private val top: Int,
     @DimenRes private val bottom: Int,
     @DimenRes private val start: Int,
     @DimenRes private val end: Int
-): RecyclerView.ItemDecoration() {
+) : RecyclerView.ItemDecoration() {
+
+    constructor(
+        @DimenRes horizontal: Int = R.dimen.no_margin,
+        @DimenRes vertical: Int = R.dimen.no_margin
+    ) : this(
+        top = vertical,
+        bottom = vertical,
+        start = horizontal,
+        end = horizontal
+    )
+
     override fun getItemOffsets(
         outRect: Rect,
         view: View,
