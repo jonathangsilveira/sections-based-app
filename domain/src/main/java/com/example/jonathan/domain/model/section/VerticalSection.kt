fe074bdb247299326d7cddb6a281c8081afc1475
Jonathan Giorgi Silveira <jonathan.silveira-ext@ab-inbev.com>
2022-11-05 22:15:20 -0300

    Add vertical section model

---
+++ domain/src/main/java/com/example/jonathan/domain/model/section/VerticalSection.kt
@@ -0,0 +1,10 @@
+package com.example.jonathan.domain.model.section
+
+import com.example.jonathan.domain.model.item.SectionItem
+import com.example.jonathan.domain.model.properties.Header
+
+data class VerticalSection(
+    override val id: Int,
+    override val header: Header?,
+    override val items: List<SectionItem>
+) : Section
