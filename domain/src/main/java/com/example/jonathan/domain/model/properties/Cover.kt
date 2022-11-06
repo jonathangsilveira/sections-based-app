a9600d9dc465f5606d72e83cac3ce8314e9f5820
Jonathan Giorgi Silveira <jonathan.silveira-ext@ab-inbev.com>
2022-11-05 22:17:34 -0300

    Remove unused properties to models

--- domain/src/main/java/com/example/jonathan/domain/model/properties/Cover.kt
+++ domain/src/main/java/com/example/jonathan/domain/model/properties/Cover.kt
@@ -2,7 +2,5 @@ package com.example.jonathan.domain.model.properties
 
 data class Cover(
     val url: String?,
-    val shape: Shape,
-    val rounded: Boolean,
     val size: SizeType
 ): Element
--- domain/src/main/java/com/example/jonathan/domain/model/section/HorizontalSection.kt
+++ domain/src/main/java/com/example/jonathan/domain/model/section/HorizontalSection.kt
@@ -3,11 +3,8 @@ package com.example.jonathan.domain.model.section
 import com.example.jonathan.domain.model.item.SectionItem
 import com.example.jonathan.domain.model.properties.Header
 
-data class LinearSection(
+data class HorizontalSection(
     override val id: Int,
     override val header: Header?,
-    override val items: List<SectionItem>,
-    val orientation: Orientation = Orientation.VERTICAL
-) : Section {
-    enum class Orientation { VERTICAL, HORIZONTAL }
-}
+    override val items: List<SectionItem>
+) : Section
