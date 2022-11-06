be6639ea861b7fad295d3337d1f97277da31d82c
Jonathan Giorgi Silveira <jonathan.silveira-ext@ab-inbev.com>
2022-11-05 22:16:14 -0300

    Add subtitle property to model; Change title type from Text to nullable String

--- domain/src/main/java/com/example/jonathan/domain/model/item/RecentlyPlayedItem.kt
+++ domain/src/main/java/com/example/jonathan/domain/model/item/RecentlyPlayedItem.kt
@@ -2,12 +2,10 @@ package com.example.jonathan.domain.model.item
 
 import com.example.jonathan.domain.model.properties.Cover
 import com.example.jonathan.domain.model.properties.SizeType
-import com.example.jonathan.domain.model.properties.Text
 
-data class DefaultItem(
+data class RecentlyPlayedItem(
     override val id: Int,
     override val cover: Cover,
     override val size: SizeType,
-    override val title: Text,
-    val subtitle: Text?
+    override val title: String?
 ): SectionItem
