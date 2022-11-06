90597629e2e76946c7048f0b04b9356a23faacf3
Jonathan Giorgi Silveira <jonathan.silveira-ext@ab-inbev.com>
2022-11-05 22:17:12 -0300

    Change type from Text to String

--- domain/src/main/java/com/example/jonathan/domain/model/item/SectionItem.kt
+++ domain/src/main/java/com/example/jonathan/domain/model/item/SectionItem.kt
@@ -2,11 +2,10 @@ package com.example.jonathan.domain.model.item
 
 import com.example.jonathan.domain.model.properties.Cover
 import com.example.jonathan.domain.model.properties.SizeType
-import com.example.jonathan.domain.model.properties.Text
 
 interface SectionItem {
     val id: Int
     val cover: Cover
     val size: SizeType
-    val title: Text
+    val title: String?
 }
