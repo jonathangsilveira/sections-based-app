bb3312cee1dce227e0c33410b720eab0422f54f3
Jonathan Giorgi Silveira <jonathan.silveira-ext@ab-inbev.com>
2022-12-05 23:56:06 -0300

    Add feature koin module provider

---
+++ app/src/main/java/com/example/jonathan/sectionsapp/di/HomeModuleProvider.kt
@@ -0,0 +1,35 @@
+package com.example.jonathan.sectionsapp.di
+
+import com.example.jonathan.component.ViewHolderComponent
+import com.example.jonathan.domain.mapper.Mapper
+import com.example.jonathan.domain.model.item.SectionItem
+import com.example.jonathan.domain.model.properties.ItemType
+import com.example.jonathan.domain.repository.HomeRepository
+import com.example.jonathan.sectionsapp.HomeViewModel
+import com.example.jonathan.sectionsapp.domain.mapper.HeaderMapper
+import com.example.jonathan.sectionsapp.domain.mapper.RecentlyPlayedMapper
+import com.example.jonathan.sectionsapp.domain.mapper.SectionsMapper
+import com.example.jonathan.sectionsapp.domain.usecase.GetHomeUseCase
+import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.dsl.module
+
+val homeModule = module {
+    viewModel {
+        HomeViewModel(
+            getHomeUseCase = GetHomeUseCase(
+                repository = get<HomeRepository>(),
+                mapper = get<SectionsMapper>()
+            )
+        )
+    }
+
+    @Suppress("UNCHECKED_CAST")
+    factory {
+        SectionsMapper(
+            headerMapper = HeaderMapper(),
+            itemMapper = mapOf(
+                ItemType.RECENTLY_PLAYED to RecentlyPlayedMapper()
+            ) as Map<ItemType, Mapper<SectionItem, ViewHolderComponent?>>
+        )
+    }
+}
