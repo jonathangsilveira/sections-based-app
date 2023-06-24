aa0d9d31bffc3c076eec47da7fbe9973d66c14b9
Jonathan Giorgi Silveira <jonathan.silveira-ext@ab-inbev.com>
2023-06-24 17:25:04 -0300

    Rename api key constant

--- data/src/main/java/com/example/jonathan/data/Back4AppInterceptor.kt
+++ data/src/main/java/com/example/jonathan/data/Back4AppInterceptor.kt
@@ -3,8 +3,8 @@ package com.example.jonathan.data
 import okhttp3.Interceptor
 import okhttp3.Response
 
-private const val KEY = "{API_KEY}"
-private const val APP_ID = "{APPLICATION_ID}"
+private const val API_KEY = ""
+private const val APP_ID = ""
 private const val APPLICATION_ID_HEADER = "X-Parse-Application-Id"
 private const val REST_API_KEY_HEADER = "X-Parse-REST-API-Key"
 
@@ -13,7 +13,7 @@ internal class Back4AppInterceptor: Interceptor {
         val request = chain.request()
             .newBuilder()
             .addHeader(APPLICATION_ID_HEADER, APP_ID)
-            .addHeader(REST_API_KEY_HEADER, KEY)
+            .addHeader(REST_API_KEY_HEADER, API_KEY)
             .build()
         return chain.proceed(request)
     }
