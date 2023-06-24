bcc13ca9e297e8c7e1e0183d683ccef9991b84cc
Jonathan Giorgi Silveira <jonathan.silveira-ext@ab-inbev.com>
2023-06-24 17:14:57 -0300

    Remove unused classes

--- app/src/main/java/com/example/jonathan/sectionsapp/component/HomeItemEvent.kt
+++
@@ -1,9 +0,0 @@
-package com.example.jonathan.sectionsapp.component
-
-import com.example.jonathan.component.ViewHolderItemEvent
-
-sealed class HomeItemEvent: ViewHolderItemEvent {
-    object ItemClicked: HomeItemEvent()
-    object ItemLongClicked: HomeItemEvent()
-    object RemoveItemClicked: HomeItemEvent()
-}
