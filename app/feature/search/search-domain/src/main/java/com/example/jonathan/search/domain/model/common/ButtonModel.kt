6825001f22131e45e7cdbe4b077d90d525bfa84a
Jonathan Giorgi Silveira <jonathan.silveira-ext@ab-inbev.com>
2023-07-23 15:29:31 -0300

    Make ButtonModel a NavigableModel

--- app/feature/search/search-domain/src/main/java/com/example/jonathan/search/domain/model/common/ButtonModel.kt
+++ app/feature/search/search-domain/src/main/java/com/example/jonathan/search/domain/model/common/ButtonModel.kt
@@ -2,5 +2,5 @@ package com.example.jonathan.search.domain.model.common
 
 data class ButtonModel(
     val text: String,
-    val navigation: NavigationModel?,
-)
+    override val navigation: NavigationModel,
+) : NavigableModel
